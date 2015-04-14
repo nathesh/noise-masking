20817b3d8b3d248123c9ef4dad406704979f1463
nathesh <thejchess@gmail.com>
2015-04-14 00:33:11 -0500

    merging files

--- summation.c
+++ summation.c
@@ -23,6 +23,8 @@ typedef struct
 #define CHANNELS (2)
 #define SAMPLE_RATE (8000)
 #define CHECK_OVERFLOW  (0)
+#define LINEAR (1) //0 for octave bands
+#define NUM_BANDS (10)
 int read_write_streams(void);
 data* output_file(void);
 
@@ -58,6 +60,92 @@ static int output_callback(const void *inputBuffer, void *outputBuffer,
   return  paContinue;
   }
 
+  //converts microphone data to mono and converts to fftw_complex type 
+void inputsignal(fftw_complex* signal,float* Record, int numsamples) {
+  int k;  
+ // int i; //counter
+ //i dont know if this will work if numsamples is odd....
+  for (k = 0; k < numsamples; k++) {
+       if(CHANNELS == 2){
+         if(k%2 == 0){
+           signal[k/2][0] = (Record[k]+Record[k+1])/2;
+        //   printf("%f,\n",Record[k]);
+           signal[k/2][1] = 0;
+         }
+       }
+         else{
+           signal[k][0] = Record[k];
+      //     printf("%f,\n",Record[k]);
+           signal[k][1] = 0;
+       } 
+      //  printf("%f \n",signal[k][0]);
+  }
+}
+
+void A_compute_coeff(int n, float* A, float fres) {
+  int i;
+  int freq, freq1; //or float???? lose precision....
+  float Y1 = pow(12200,2);
+  float Y2 = pow(20.6,2);
+  float Y3 = pow(107.7,2);
+  float Y4 = pow(737.9,2);
+  for (i=0; i < n; i++) 
+{      freq1 = fres*i;
+       freq = pow(i * fres,2);
+       A[i] =20*log10((Y1 *pow(freq,2))/ 
+              ((Y1 + freq) * (Y2 + freq) *  sqrt((Y3 + freq) * (Y4 + freq))))+2;
+      // printf("freq:%3d  %12f \n",freq1,A[i]);
+    }
+}
+
+void apply_weighting(int n, float* weights, float* in)
+{
+  int i;
+  for(i=0; i<n; i++){
+  //  printf("%f\n",weights[i]);
+    in[i] = weights[i] + in[i];
+  }
+}
+
+void compute_band_weights(int n, float* in, float fres,float* out, float* bands)
+{
+  //in is PSD data 
+  //fres * idx = frequency ithink????
+  //out is array of 10 values (NUM_BANDS) 
+  //n  
+  //compute average over each octave band 
+  //map values from 0-1 
+  //print out weights for debuggin
+  int i,k,count;
+  count = 0;
+ // int bands = (SAMPLE_RATE/2)/NUM_BANDS
+//go through and find indices of bands
+//calculate average in each band  
+  i = 1;
+  for (k = 0; k < n; k++) {
+      if ((bands[i-1] <= (k*fres)) && ((k*fres) <= bands[i])) {
+         out[i-1] += in[k];
+         count ++;
+      } 
+      else if (LINEAR && ((k*fres)<bands[0])) {
+        // if between -0 80 in linear bands ignor 
+      }   
+      else {
+         out[i-1] /= count;
+        // printf("%d\n",count);
+         count = 0;
+         i ++; 
+      }
+  }
+  for (i =0 ; i < NUM_BANDS; i++){
+  //   printf("bands:%f average:%f\n",bands[i],out[i]);
+  }
+
+//normalize to 0-1 and add up to 1...
+  
+}
+
+
 int main(void)
 {
 
@@ -74,9 +162,10 @@ int read_write_streams(void)
 	PaStreamParameters input, output; 
 	PaStream *stream_input,  *stream_output; 
 	PaError error_input,error_output;
-	float *recordsamples,summation;
-	int i,totalframes,numsamples,numbytes,y; 
-	data* struct_data;
+	float den, fres, *recordsamples,*powerspec, *A, *weights, *bands, summation;
+	int i,totalframes,numsamples,numbytes,y; //y might cause problems 
+	ftw_complex *in, *out;
+  fftw_plan plan;data* struct_data;
 	/* Declaration */
 	
 	struct_data = (data*) malloc(sizeof(data));
@@ -87,16 +176,45 @@ int read_write_streams(void)
   /* Read the wav */
 
 	/* Intialization */
-	totalframes   = 5*SAMPLE_RATE;
-	numsamples    = CHANNELS*totalframes;
-	numbytes  	  = numsamples * sizeof(float);
-	recordsamples = (float*) malloc(numbytes);
-	for (i = 0; i < numsamples; i++)
-	{
-		recordsamples[i] = 0;
-		
-	}
-	  for(i = 0;i<struct_data->num_frames*2;i++) // is accessing num_frames bad?
+  totalframes   = CHANNELS*FRAMES;//5*SAMPLE_RATE; why????
+  numsamples    = FRAMES; //why totalframes is 5*sample rate change to totalframes if wrong 
+  fres          = (float) SAMPLE_RATE/FRAMES/2;
+  numbytes      = numsamples * sizeof(float);
+  recordsamples = (float*) malloc(numbytes);
+  CHECK_MALLOC(recordsamples,"read_write_streams");
+  powerspec = (float*) malloc(numbytes); //malloc too much memory but i think that cool we will figure it out
+  CHECK_MALLOC(powerspec,"read_write_streams"); 
+  A = (float*) malloc(numbytes);
+  CHECK_MALLOC(A,"read_write_streams");
+  weights = (float*)malloc(sizeof(float)*NUM_BANDS); //final computed band weights
+  CHECK_MALLOC(weights,"read_write_streams");
+  bands = (float*)malloc(sizeof(float)*NUM_BANDS);  //bands specified to compute band weights
+  CHECK_MALLOC(bands,"read_write_streams");
+  in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * numsamples);
+  out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * numsamples);
+  CHECK_MALLOC(in,"read_write_streams");
+  CHECK_MALLOC(out,"read_write_streams");
+  plan = fftw_plan_dft_1d(numsamples, in, out, FFTW_FORWARD, FFTW_MEASURE);
+  struct_data->cursor = 0;
+  struct_data->num_frames = 441000;
+  for (i = 0; i < numsamples; i++){
+    recordsamples[i] = 0;
+    powerspec[i] = 0; //should be half the size of the recorded samples
+    A[i] = 0;
+  }
+  for (i = 0; i < NUM_BANDS; i++){
+    weights[i] = 1;  //init weights to 1 equal volume 
+  }
+  if (LINEAR == 1){
+     bands[0] = 80;
+     for (i = 1; i <= NUM_BANDS; i++) {
+         bands[i] = i*(SAMPLE_RATE/2)/NUM_BANDS;
+     }
+  }
+  else{
+  //needs to be donei for octave bands
+  }
+  for(i = 0;i<struct_data->num_frames*2;i++) // is accessing num_frames bad?
   {
     summation = 0;
     for(y = 0; y<11;y++)
@@ -105,6 +223,7 @@ int read_write_streams(void)
     }
     struct_data->data[i] = summation;
   }
+  A_compute_coeff(numsamples,A,fres);
 
 	/* Port Audio Intialization */
 	error_input = Pa_Initialize();
@@ -176,15 +295,23 @@ int read_write_streams(void)
    	  //printf("HERE!\n");
       //error_output = Pa_WriteStream(stream_output,outputsamples, 441000);
       if( error_output  != paNoError && 0) goto error_o;
-      //printf("HEREReading\n");
-      //struct_data->data = NULL; 
-
-      
-      error_input = Pa_ReadStream(stream_input,recordsamples, FRAMES);
-      if(error_input != paNoError && 0) goto error;
-
+ //do FFT PROCESSING
+      inputsignal(in, recordsamples, CHANNELS*numsamples); //converts to fftw_complex and averages stereo to mono
+      weighted_power_spectrum_fftw(numsamples,in,out,powerspec,A,den,4, plan);
+      compute_band_weights(numsamples,powerspec,fres,weights,bands);
+      //print data
+      /*
+      for(i=0; i<numsamples; i++){
+       
+     //    printf("index:%d freq:%f value:%f\n",i,fres*(float)i,powerspec[i]);
+      } 
+      */
    }
    free(recordsamples);
+   free(in);
+   free(out);
+  //clean up other stuff fft and other malloced items
+
 	/* Read and Write */
 error:
 
